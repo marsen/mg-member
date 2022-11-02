9409642a351e31428f0c34c416d291e961941731
Mark Lin <mark.lin@ttfb-lab.com>
2022-11-02 19:34:37 +0800

    add gin sample ping pong

--- main.go
+++ main.go
@@ -1,7 +1,18 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"github.com/gin-gonic/gin"
+	"net/http"
+)
 
 func main() {
 	fmt.Printf("Hello Marsen Go Member")
+	r := gin.Default()
+	r.GET("/ping", func(c *gin.Context) {
+		c.JSON(http.StatusOK, gin.H{
+			"message": "pong",
+		})
+	})
+	r.Run() // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
 }
